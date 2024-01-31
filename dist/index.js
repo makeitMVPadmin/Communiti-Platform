d0dfa9c1e1fd7008831452b1bb7a296fd2807616
felixreynoso <engfelixreynoso@gmail.com>
2024-01-31 10:06:09 -0800

    feat: initial setup complete

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+console.log("x");
+const res = 1;
+console.log(res);
+function sum(a, b) {
+    return a + b;
+}
+sum(1, 2);
---
+++ src/index.ts
@@ -0,0 +1,11 @@
+console.log("x");
+
+const res = 1;
+
+console.log(res);
+
+function sum(a: number, b: number) {
+  return a + b;
+}
+
+sum(1, 2);
