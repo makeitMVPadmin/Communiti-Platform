6b2c812e040a4b471de602319847e735ab56f230
rach638 <rachelkellner1@gmail.com>
2024-02-13 16:26:35 -0500

    feat: first draft communities entity

--- functions/src/routes/router.js
+++ functions/src/routes/router.js
@@ -2,10 +2,12 @@ const express = require("express");
 
 const eventsRouter = require("./eventsRouter");
 const userRouter = require("./userRouter");
+const communitiesRouter = require("./communitiesRouter");
 
 const router = express.Router();
 
 router.use("/events", eventsRouter);
 router.use("/user", userRouter);
+router.use("/communities", communitiesRouter);
 
 module.exports = router;
