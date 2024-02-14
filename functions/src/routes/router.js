6222bb1145cb256449dbbf0d020a5bb7abfa2317
andrewalson <83783605+andrewalson@users.noreply.github.com>
2024-02-14 17:36:20 -0500

    feat: add messages entity including router and controller files

--- functions/src/routes/router.js
+++ functions/src/routes/router.js
@@ -2,10 +2,12 @@ const express = require("express");
 
 const eventsRouter = require("./eventsRouter");
 const userRouter = require("./userRouter");
+const messagesRouter = require("./messagesRouter");
 
 const router = express.Router();
 
 router.use("/events", eventsRouter);
 router.use("/user", userRouter);
+router.use("/messages", messagesRouter);
 
 module.exports = router;
