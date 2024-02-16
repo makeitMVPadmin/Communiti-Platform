6003153de15937e013522408636f0f5e58ebff35
rach638 <rachelkellner1@gmail.com>
2024-02-16 09:44:06 -0800

    completed refactoring for my controllers

--- functions/src/routes/router.js
+++ functions/src/routes/router.js
@@ -1,13 +1,13 @@
 const express = require("express");
 
 const eventsRouter = require("./eventsRouter");
-const userRouter = require("./userRouter");
+const usersRouter = require("./usersRouter");
 const communitiesRouter = require("./communitiesRouter");
 
 const router = express.Router();
 
 router.use("/events", eventsRouter);
-router.use("/user", userRouter);
+router.use("/users", usersRouter);
 router.use("/communities", communitiesRouter);
 
 module.exports = router;
