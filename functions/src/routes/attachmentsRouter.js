9f31e48a8d5acb05229ef9c27743791a975a9d31
andrewalson <83783605+andrewalson@users.noreply.github.com>
2024-02-16 11:03:56 -0500

    feat: add support for attachments entity controller with controller, router, and generated swagger docs

---
+++ functions/src/routes/attachmentsRouter.js
@@ -0,0 +1,26 @@
+const express = require("express");
+
+const attachmentsController = require("../controllers/attachmentsController.js");
+
+const attachmentsRouter = express.Router();
+
+attachmentsRouter.get(
+  "/:attachmentId",
+  attachmentsController.getSingleAttachment
+);
+
+attachmentsRouter.get("/", attachmentsController.getAllattachments);
+
+attachmentsRouter.patch(
+  "/:attachmentId",
+  attachmentsController.updateAttachment
+);
+
+attachmentsRouter.post("/", attachmentsController.addAttachment);
+
+communitiesRouter.delete(
+  "/:attachmentId",
+  attachmentsController.deleteAttachment
+);
+
+module.exports = attachmentsRouter;
--- functions/src/routes/router.js
+++ functions/src/routes/router.js
@@ -3,11 +3,13 @@ const express = require("express");
 const eventsRouter = require("./eventsRouter");
 const userRouter = require("./userRouter");
 const communitiesRouter = require("./communitiesRouter");
+const attachmentsRouter = require("./attachmentsRouter");
 
 const router = express.Router();
 
 router.use("/events", eventsRouter);
 router.use("/user", userRouter);
 router.use("/communities", communitiesRouter);
+router.use("/attachments", attachmentsRouter);
 
 module.exports = router;
